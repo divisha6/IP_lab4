49cbcb7f59ed6ae81d5fdc36992c3d3cfa89b3ef
divisha6 <92202583+divisha6@users.noreply.github.com>
2022-09-18 22:00:18 +0530

    Update index.js

--- tshirt/index.js
+++ tshirt/index.js
@@ -2,10 +2,8 @@ function validateForm() {
     // Part A
     const values = [
         document.forms["myForm"]["custom"].value,
-        document.forms["myForm"]["text-tshirt"].value,
         document.forms["myForm"]["size"].value,
         document.forms["myForm"]["qty"].value,
-        document.forms["myForm"]["details"].value,
         document.forms["myForm"]["date"].value,
         document.forms["myForm"]["fname"].value,
         document.forms["myForm"]["lname"].value,
@@ -15,29 +13,21 @@ function validateForm() {
         document.forms["myForm"]["color"].value,
         document.forms["myForm"]["payment"].value,
     ];
-    const hasEmptyFields = Object.values(values).some(
-        (element) => element === ""
-    );
-    if (hasEmptyFields) {
-        alert("Please fill in all fields");
-        return;
-    }
 
-const receipt_formatted = `Thank you for shopping!
+    const receipt_formatted = `Thank you for shopping!
 Here's your receipt:
 Date: ${new Date().toLocaleDateString("UK")}
 Custom Message: ${values[0]}
 Size: ${values[1]}
 Quantity: ${values[2]}
-Details: ${values[3]}
-Date: ${values[4]}
-First Name: ${values[5]}
-Last Name: ${values[6]}
-Address: ${values[7]}
-Contact: ${values[8]}
-Email: ${values[9]}
-Payment Method: ${values[10]}
-Color: ${values[11]}`;
+Date: ${values[3]}
+First Name: ${values[4]}
+Last Name: ${values[5]}
+Address: ${values[6]}
+Contact: ${values[7]}
+Email: ${values[8]}
+Payment Method: ${values[9]}
+Color: ${values[10]}`;
     alert(receipt_formatted);
 }
 const form = document.getElementById("place_order");
